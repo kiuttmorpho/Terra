52f81191b565edb92b77eea30e5d30a70142e729
Evilbonesss <stepanpopov2005@gmail.com>
2025-02-26 13:13:40 +0300

    Model part

--- src/main/java/mephi/b23902/i/terra/DataGenerator.java
+++ src/main/java/mephi/b23902/i/terra/DataGenerator.java
@@ -4,10 +4,69 @@
  */
 package mephi.b23902.i.terra;
 
+import com.github.javafaker.Faker;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
 /**
- *
- * @author tsyga
+ * Это для генерации тестовых данных о еретиках
  */
 public class DataGenerator {
+    private final Faker faker;
+    private final Random random;
     
+    /**
+     * Это конструктор класса DataGenerator
+     */
+    public DataGenerator() {
+        this.faker = new Faker(new Locale("ru"));
+        this.random = new Random();
+    }
+    
+    /**
+     * Генерация случайного объекта Heretic
+     */
+    public Heretic generateHeretic() {
+        String name = faker.name().fullName();
+        
+        // Генерируем преступления
+        String[] possibleCrimes = {
+            "Ересь", "Мятеж", "Отрицание Императора", "Распространение запрещенных знаний",
+            "Связь с ксеносами", "Использование запрещенной технологии", "Хранение архео-технологии",
+            "Саботаж", "Дезертирство", "Подстрекательство к бунту"
+        };
+        
+        StringBuilder crimes = new StringBuilder();
+        int numCrimes = random.nextInt(3) + 1; // От 1 до 3 преступлений
+        
+        for (int i = 0; i < numCrimes; i++) {
+            if (i > 0) {
+                crimes.append(", ");
+            }
+            crimes.append(possibleCrimes[random.nextInt(possibleCrimes.length)]);
+        }
+        
+        String lastLocation = faker.address().cityName() + ", сектор " + 
+                              faker.address().buildingNumber();
+        
+        String identityId = "IMP-" + faker.numerify("######-##");
+        
+        Heretic.ThreatLevel[] threatLevels = Heretic.ThreatLevel.values();
+        Heretic.ThreatLevel threatLevel = threatLevels[random.nextInt(threatLevels.length)];
+        
+        return new Heretic(name, crimes.toString(), lastLocation, identityId, threatLevel);
+    }
+    
+    /**
+     * Генерирует список еретиков заданного размера
+     */
+    public List<Heretic> generateHereticList(int count) {
+        List<Heretic> heretics = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            heretics.add(generateHeretic());
+        }
+        return heretics;
+    }
 }
--- src/main/java/mephi/b23902/i/terra/Heretic.java
+++ src/main/java/mephi/b23902/i/terra/Heretic.java
@@ -4,10 +4,81 @@
  */
 package mephi.b23902.i.terra;
 
-/**
- *
- * @author tsyga
- */
 public class Heretic {
+    private String name;
+    private String crimes;
+    private String lastLocation;
+    private String identityId;
+    private ThreatLevel threatLevel;
+
     
+    public enum ThreatLevel {
+        LOW("Низкий"),
+        MEDIUM("Средний"),
+        HIGH("Высокий"),
+        CRITICAL("Критический");
+        
+        private final String description;
+        
+        ThreatLevel(String description) {
+            this.description = description;
+        }
+        
+        public String getDescription() {
+            return description;
+        }
+    }
+    
+    public Heretic(String name, String crimes, String lastLocation, String identityId, ThreatLevel threatLevel) {
+        this.name = name;
+        this.crimes = crimes;
+        this.lastLocation = lastLocation;
+        this.identityId = identityId;
+        this.threatLevel = threatLevel;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getCrimes() {
+        return crimes;
+    }
+    
+    public void setCrimes(String crimes) {
+        this.crimes = crimes;
+    }
+    
+    public String getLastLocation() {
+        return lastLocation;
+    }
+    
+    public void setLastLocation(String lastLocation) {
+        this.lastLocation = lastLocation;
+    }
+    
+    public String getIdentityId() {
+        return identityId;
+    }
+    
+    public void setIdentityId(String identityId) {
+        this.identityId = identityId;
+    }
+    
+    public ThreatLevel getThreatLevel() {
+        return threatLevel;
+    }
+    
+    public void setThreatLevel(ThreatLevel threatLevel) {
+        this.threatLevel = threatLevel;
+    }
+    
+    @Override
+    public String toString() {
+        return "Еретик: " + name + " | Уровень угрозы: " + threatLevel.getDescription();
+    }
 }
