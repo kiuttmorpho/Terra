eb3e39fd496ab75f846362689d1ab15a9be71c64
kiuttmorpho <tsygankova.elizaveta@gmail.com>
2025-02-26 12:28:44 +0300

    create

---
+++ src/main/java/mephi/b23902/i/terra/Terra.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b23902.i.terra;
+
+/**
+ *
+ * @author tsyga
+ */
+public class Terra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
